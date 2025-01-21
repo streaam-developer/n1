17ad305187d9b187e7f7fed10cf5d1f6435426c6
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2025-01-21 16:52:35 +0530

    Update utils.py

--- mfinder/utils/utils.py
+++ mfinder/utils/utils.py
@@ -7,6 +7,7 @@ class temp(object):
 import logging
 from pyrogram.errors import UserNotParticipant
 from mfinder.db.broadcast_sql import check_join_request
+from mfinder import *
 
 # Initialize logger
 logger = logging.getLogger(__name__)
@@ -18,6 +19,7 @@ async def is_subscribed(bot, query, channel_id, invite_link):
     Check if a user is subscribed to the given channel.
     If not subscribed, send the invite link.
     """
+    invite_link = AUTH_LINK
     try:
         # Check if the user is a member of the channel
         user = await bot.get_chat_member(channel_id, query.from_user.id)
