f1aac6d2fd01f0aa5f74d8d8d505a4446219766e
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2025-01-21 16:49:36 +0530

    s

--- mfinder/plugins/join_req.py
+++ mfinder/plugins/join_req.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from mfinder.db.broadcast_sql import check_join_request, add_join_request, delete_all_join_requests
-from mfinder import ADMINS, SECOND_AUTH_CHANNEL, THIRD_AUTH_CHANNEL
+from mfinder import ADMINS, SECOND_AUTH_CHANNEL, THIRD_AUTH_CHANNEL, AUTH_LINK
 
 from mfinder.utils.utils import *
 FSUB_CHANNELS = [SECOND_AUTH_CHANNEL, THIRD_AUTH_CHANNEL]
@@ -9,21 +9,17 @@ FSUB_CHANNELS = [SECOND_AUTH_CHANNEL, THIRD_AUTH_CHANNEL]
 @Client.on_chat_join_request(filters.group | filters.channel)
 async def autoapprove(client: Client, message: ChatJoinRequest):
     user = message.from_user
-
-    # Add to the new join_requests table
-    if not await check_join_request(user.id, message.chat.id):
-        await add_join_request(user.id, message.chat.id)
-
     all_joined = True
+
+    # Use the permanent invite link for all required channels
     for channel_id in FSUB_CHANNELS:
-        if not await is_subscribed(client, message, [channel_id]):
+        if not await is_subscribed(client, message, channel_id, AUTH_LINK):
             all_joined = False
             break
 
+    # If the user is subscribed to all channels, approve the request
     if all_joined:
-        dl = temp.DEL_MSG.get(user.id)
-        if dl:
-            await dl.delete()
+        await client.approve_chat_join_request(chat_id=message.chat.id, user_id=user.id)
 
 @Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
 async def del_requests(client, message):
--- mfinder/utils/utils.py
+++ mfinder/utils/utils.py
@@ -11,41 +11,26 @@ from mfinder.db.broadcast_sql import check_join_request
 # Initialize logger
 logger = logging.getLogger(__name__)
 
-async def is_subscribed(bot, query, FSUB_CHANNELS):
+from pyrogram.errors import UserNotParticipant
+
+async def is_subscribed(bot, query, channel_id, invite_link):
     """
-    Check if a user is subscribed to all channels listed in FSUB_CHANNELS.
-
-    Args:
-        bot: The Pyrogram bot instance.
-        query: The callback query or message containing the user information.
-        FSUB_CHANNELS (list): List of channel IDs to check for subscription.
-
-    Returns:
-        bool: True if the user is subscribed to all channels, False otherwise.
+    Check if a user is subscribed to the given channel.
+    If not subscribed, send the invite link.
     """
     try:
-        for channel in FSUB_CHANNELS:  # Iterate over each channel
-            # Check if there is a join request in the current channel
-            join_request_exists = await check_join_request(query.from_user.id, int(channel))
-
-            if join_request_exists:
-                continue  # If join request exists, move to the next channel
-
-            try:
-                # Check if the user is a member of the current channel
-                user = await bot.get_chat_member(int(channel), query.from_user.id)
-            except UserNotParticipant:
-                return False  # If the user is not a participant in any one channel, return False
-            except Exception as e:
-                logger.exception(f"Error checking membership for channel {channel}: {e}")
-                return False  # Return False if any other error occurs
-
-            # If the user is banned ('kicked'), return False
-            if user.status == 'kicked':
-                return False
-
+        # Check if the user is a member of the channel
+        user = await bot.get_chat_member(channel_id, query.from_user.id)
+        if user.status in ["member", "administrator", "creator"]:
+            return True  # User is subscribed
+    except UserNotParticipant:
+        # User is not a participant, send the invite link
+        await query.message.reply_text(
+            f"Please join this channel to continue: [Join Here]({invite_link})",
+            disable_web_page_preview=True,
+        )
+        return False
     except Exception as e:
-        logger.exception(f"Unexpected error in is_subscribed: {e}")
-        return False  # In case of any other error, return False
-
-    return True  # If the user is subscribed to all channels, return True
+        # Handle any other exceptions
+        print(f"Error checking subscription: {e}")
+        return False
